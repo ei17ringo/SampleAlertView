1eee53ddf3264464ebd5b0534c73929ede1752a4
ei17ringo <ei17ringo@gmail.com>
2014-07-11 09:17:04 +0800

    Initial Commit

---
+++ SampleAlertView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleAlertView
+//
+//  Created by Eriko Ichinohe on 2014/07/11.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
