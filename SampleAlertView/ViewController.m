634b19d46c9920172d19637246721e96ae1db7c0
ei17ringo <ei17ringo@gmail.com>
2014-08-13 11:21:08 +0800

    message若干変更

--- SampleAlertView/ViewController.m
+++ SampleAlertView/ViewController.m
@@ -18,6 +18,27 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"今日アクティビティ行きますか？"
+        message:@"行きます？"
+        delegate:self
+        cancelButtonTitle:@"Cancel"
+        otherButtonTitles:@"OK", nil];
+    
+    
+    [alert show];
+}
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+
+    if(buttonIndex == 1){
+        NSLog(@"OK");
+        self.myLabel.text = @"アクティビティ楽しんで！";
+    }else{
+        NSLog(@"Cancel");
+        self.myLabel.text = @"次回はアクティビティ行きましょう！";
+    }
 }
 
 - (void)didReceiveMemoryWarning
